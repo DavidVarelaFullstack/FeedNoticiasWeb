53f7469e72467326a23a5ecf45ede3c4ae2f1db5
DavidVarela <david@eligedata.com>
2022-04-28 11:08:34 +0200

    Cambios leves

--- resources/views/Formulario/formulario.blade.php
+++ resources/views/Formulario/formulario.blade.php
@@ -17,14 +17,14 @@
                     session()->put('Mensaje', null);
                 @endphp
             @endif
-            <label class="fw-bold required" style="font-size: 19px;">Titulo</label><br>
+            <label class="fw-bold required" style="font-size: 19px;">Titulo (Evitar escribir un título ya escrito previamente)</label><br>
             <input class="form-control" required type="text" name="titulo" id="titulo"><br>
             <label class="fw-bold required" style="font-size: 19px;">Descripción</label><br>
             <textarea maxlength="255" cols="40" rows="8" name="descripcion" id="descripcion"></textarea><br>
             <label class="fw-bold required" style="font-size: 19px;">Enlace</label><br>
             <input class="form-control" required type="text" name="enlace" id="enlace"><br>
-            <label class="fw-bold" style="font-size: 19px;">Imagen (<span class="text-decoration-underline">200x200 para mayor calidad</span>)</label><br>
-            <input class="form-control" accept="image/png,image/jpg,image/jpeg" type="file" name="imagen" id="imagen"><br>
+            <label class="fw-bold required" style="font-size: 19px;">Imagen (<span class="text-decoration-underline">que sea cuadrada para mayor calidad, ejemplo: 800x800</span>)</label><br>
+            <input required class="form-control" accept="image/png,image/jpg,image/jpeg" type="file" name="imagen" id="imagen"><br>
             <label class="fw-bold required" style="font-size: 19px;">Fecha</label><br>
             <input class="form-control" required class="form-control" required type="date" name="fecha" id="fecha"><br>
             <button class="d-block mx-auto btn btn-outline-light" type="submit">Enviar noticia</button>
